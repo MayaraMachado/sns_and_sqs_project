90e3e1f1164e02fe6e3f1017b1593cc67e5b411e
Mayara Machado <may.mchdo@gmail.com.br>
2020-07-21 10:40:02 -0300

    Rebase com a master

--- ecommerce/api/models.py
+++ ecommerce/api/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 class Seller(models.Model):
     seller_id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
